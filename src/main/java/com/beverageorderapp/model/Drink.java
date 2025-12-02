b2f593bd45c5e8c8b1a8f4c41b9aa2cb12966bc2
hung <hung@kunzendorf-spedition.de>
2025-12-02 15:42:53 +0100

    feat(model): add Drink and DrinkFactory as base for Factory Method design pattern

---
+++ src/main/java/com/beverageorderapp/model/Drink.java
@@ -0,0 +1,6 @@
+package main.java.com.beverageorderapp.model;
+
+public interface Drink {
+    double getCost();
+    String getDescription();
+}
---
+++ src/main/java/com/beverageorderapp/model/DrinkFactory.java
@@ -0,0 +1,6 @@
+package main.java.com.beverageorderapp.model;
+
+public abstract class DrinkFactory {
+    protected abstract Drink createDrink();
+    public void someOperation(){};
+}
